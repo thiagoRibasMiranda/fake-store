de91b6451f0a7bf76e36824f5cec272b1536c986
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2022-12-23 19:03:17 -0300

    feat: create component Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import {
+  Center,
+  Flex,
+  Heading,
+} from '@chakra-ui/react';
+import { ButtonCartShopping } from './ButtonCartShopping';
+
+export function Header(): JSX.Element {
+  return (
+    <Flex justify="space-between" p="2" borderBottom="1px" borderColor="gray.200">
+      <Center>
+        <Heading as="h3" size="sm" color="blue.700">Fake Store</Heading>
+      </Center>
+      <ButtonCartShopping />
+    </Flex>
+  );
+}
