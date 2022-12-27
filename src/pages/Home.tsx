d46c02ee55bd3c7494bec99dcd642a10c711e14e
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2022-12-27 12:01:41 -0300

    feat: add components to page Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,28 +1,34 @@
 import * as React from 'react';
-import {
-  Text,
-} from '@chakra-ui/react';
+import { Grid } from '@chakra-ui/react';
 import getAllProducts from '../services/getAllProducts';
 import Product from '../ProductInterface';
-import { Loading } from '../components/Loading';
 import { Header } from '../components/Header';
+import { StoreCategories } from '../components/StoreCategories';
+import { ProductCard } from '../components/ProductCard';
+import { SkeletonCard } from '../components/SkeletonCard';
 
 export function Home(): JSX.Element {
   const [products, setProducts] = React.useState<Product[]>([]);
+  const [loading, setLoading] = React.useState(true);
 
   React.useEffect(() => {
     getAllProducts()
-      .then((res) => setProducts(res));
+      .then((res) => setProducts(res))
+      .then(() => setLoading(false));
   }, []);
 
   return (
     <div>
       <Header />
-      {
-        products
-          ? (products.map((product) => (<Text>{product.title}</Text>)))
-          : (<Loading />)
-      }
+      <StoreCategories />
+      <Grid templateColumns="repeat(5, 1fr)" gap={2} p="2">
+        {
+          !loading
+            ? (products.map((product) => (
+              <ProductCard product={product} key={product.id} />)))
+            : (Array.from(Array(10).keys()).map((item) => (<SkeletonCard key={item} />)))
+        }
+      </Grid>
     </div>
   );
 }
