4685d5366d61659c9dad1f8e9969bcd303a523e7
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2022-12-30 10:39:33 -0300

    feat: add GlobalContext to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,16 @@
 import * as React from 'react';
 import {
   ChakraProvider,
-  Text,
 } from '@chakra-ui/react';
-import getAllCategories from './services/getAllCategories';
 import { Home } from './pages/Home';
-import { ProductDetail } from './pages/ProductDetail';
+import GlobalContext from './context';
 
 export function App(): JSX.Element {
-  const [categories, setCategories] = React.useState<string[]>([]);
-
-  React.useEffect(() => {
-    getAllCategories()
-      .then((res) => setCategories(res));
-  }, []);
-
   return (
     <ChakraProvider>
-      <div>
+      <GlobalContext>
         <Home />
-        <ProductDetail />
-        {categories && categories.map((category) => (<Text>{category}</Text>))}
-      </div>
+      </GlobalContext>
     </ChakraProvider>
   );
 }
