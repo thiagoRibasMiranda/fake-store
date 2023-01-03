3d0f58f0135fd84a3c2816cc94eaf11d8c64d8b6
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2023-01-03 13:07:56 -0300

    feat: create component ButtonDeleteItem

---
+++ src/components/ButtonDeleteItem.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import {
+  Center,
+  CloseButton,
+} from '@chakra-ui/react';
+import { CartItensContext, CartItensContextType } from '../context/cartItensContext';
+
+interface Props {
+  itemId: number,
+}
+
+export function ButtonDeleteItem(props: Props): JSX.Element {
+  const { itemId } = props;
+  const {
+    deleteCartItem,
+  } = React.useContext(CartItensContext) as CartItensContextType;
+  const handleClick = (id: number): void => {
+    deleteCartItem(id);
+  };
+
+  return (
+    <Center>
+      <CloseButton size="md" onClick={() => handleClick(itemId)} />
+    </Center>
+  );
+}
