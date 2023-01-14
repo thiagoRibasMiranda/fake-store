99e85545a348dc9cf7bfaff3ed7beab7c25c9d65
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2023-01-14 20:54:37 -0300

    style: change heading size

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,9 +8,9 @@ import { ButtonCartShopping } from './ButtonCartShopping';
 
 export function Header(): JSX.Element {
   return (
-    <Flex justify="space-between" p="2" borderBottom="1px" borderColor="gray.200">
+    <Flex justify="space-between" width="100%" p="2" borderBottom="1px" borderColor="gray.200">
       <Center>
-        <Heading as="h3" size="sm" color="blue.800">Fake Store</Heading>
+        <Heading as="h3" size="lg" color="blue.800">Fake Store</Heading>
       </Center>
       <ButtonCartShopping />
     </Flex>
