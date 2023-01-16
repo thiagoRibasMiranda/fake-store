4080feb91b5031f78b887f9f02a8a959d1e34e36
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2023-01-16 19:20:43 -0300

    style: modify the style of the component

--- src/components/SkeletonItem.tsx
+++ src/components/SkeletonItem.tsx
@@ -1,18 +1,13 @@
 import * as React from 'react';
 import {
   Flex,
-  Center,
-  Skeleton,
   SkeletonText,
 } from '@chakra-ui/react';
 
 export function SkeletonItem(): JSX.Element {
   return (
-    <Flex direction="row" justify="space-between" align="center">
-      <Center>
-        <Skeleton height="20px" width="90%" />
-      </Center>
-      <SkeletonText mt="2" noOfLines={1} spacing="4" skeletonHeight="8" />
+    <Flex direction="column" width="100%">
+      <SkeletonText mt="2" noOfLines={1} skeletonHeight="14" />
     </Flex>
   );
 }
