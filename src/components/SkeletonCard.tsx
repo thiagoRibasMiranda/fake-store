f4acc29da322a3f9a8a94182e895491624bbacf0
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2023-01-16 19:21:12 -0300

    style: modify the style of the component

--- src/components/SkeletonCard.tsx
+++ src/components/SkeletonCard.tsx
@@ -1,19 +1,18 @@
 import * as React from 'react';
 import {
-  Box,
+  Flex,
   Center,
   Skeleton,
   SkeletonText,
-  useBreakpointValue,
 } from '@chakra-ui/react';
 
 export function SkeletonCard(): JSX.Element {
   return (
-    <Box width="150px" border="1px" borderColor="blue.800" borderRadius={useBreakpointValue({ base: 'md', md: 'xl' })}>
+    <Flex direction="column" width="240px">
       <Center>
         <Skeleton height="80px" width="90%" />
       </Center>
       <SkeletonText mt="2" noOfLines={4} spacing="4" skeletonHeight="8" />
-    </Box>
+    </Flex>
   );
 }
